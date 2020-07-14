7a48fb627c4d8f6b89460383ff01244ce0cd4f1b
Ronaldo <Ronaldo1shukulli@gmail.com>
2020-07-14 07:47:39 +0200

    Banner is now fully functional and good styling

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -21,6 +21,10 @@ const Banner = () => {
     fetchData();
   }, []);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substring(0, n - 1) + "..." : str;
+  }
+
   return (
     <header
       className="banner"
@@ -39,8 +43,11 @@ const Banner = () => {
           <button className="banner_button">My List</button>
         </div>
 
-        <h1 className="banner__description">{movie?.overview}</h1>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
+      <div className="banner__fadeBottom"></div>
     </header>
   );
 };
