a8cf2be7dfa217c9187d9702313399826efbf92d
Ronaldo <Ronaldo1shukulli@gmail.com>
2020-07-17 00:03:43 +0200

    Finish the project and deploy it to the firebese

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://assets.brand.microsites.netflix.io/assets/1ed15bca-b389-11e7-9274-06c476b5c346_cm_800w.png?v=45"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+};
+
+export default Nav;
